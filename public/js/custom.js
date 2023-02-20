a1168c7d8c6a61351fb73bbc6c30dfe026f6c048
Xope <errchosep25@gmail.com>
2023-02-20 12:39:59 +0100

    acabats dies calendari

--- public/js/custom.js
+++ public/js/custom.js
@@ -24,13 +24,51 @@ $(document).ready(function() {
 
     // Plenar dies
     (function() {
-        // 1) Mes actual
+        // const data = new Date(5 + " 1, 2023 00:00:00");
         const data = new Date();
-        const diaSetmana = data.getDay() -1;
         const mes = data.getMonth();
         const diaMes = data.getDate();
         const nDiesMes = new Date(data.getFullYear(), mes + 1, 0).getDate();
-        alert(diaSetmana + ", " + mes + ", " + diaMes + ", " + nDiesMes);
+        let dia1Mes = new Date(mes + 1 + " 1, " + data.getFullYear() + " 00:00:00").getDay();
+        dia1Mes == 0 ? dia1Mes = 6 : dia1Mes -= 1;
+        let contadorDies = 1;
+        let canviMes = false;
+        for (let i = 0; i < 6; i++) {
+            $('.calendari').append(setmanaPlantilla());
+            if(i == 5) {
+                $('.calendari__setmana:last-child').addClass('rounded-bottom');
+            }
+            for (let j = 0; j < 7; j++) {
+                let nDiesMesAnterior = new Date(data.getFullYear(), mes, 0).getDate(); // fique mes perquè el contingut de mes és el index i per tant és un menos
+                if (i == 0 && dia1Mes != 0) {
+                    if(j < dia1Mes) {
+                        $('.calendari__setmana:last-child').append(diaPlantilla({numDia : (nDiesMesAnterior - (dia1Mes - j - 1))}));
+                        $('.calendari__dia:last-child').addClass('text-muted');
+                    } else {
+                        $('.calendari__setmana:last-child').append(diaPlantilla({numDia : contadorDies}));
+                        contadorDies++;
+                    }
+                } else {
+                    $('.calendari__setmana:last-child').append(diaPlantilla({numDia : contadorDies}));
+                    if(!canviMes && diaMes == contadorDies) {
+                        $('.calendari__setmana:last-child').children('.calendari__dia:last-child').children('span').addClass('actual');
+                    }
+                    contadorDies++;
+                    if(canviMes) {
+                        $('.calendari__setmana:last-child').children('.calendari__dia:last-child').addClass('text-muted');
+                    }        
+                }
+                if(contadorDies > nDiesMes) {
+                    contadorDies = 1;
+                    canviMes = true;
+                }
+
+                // actualitzar amb el .get les tasques del dia
+                // $.get(ruta).done(function() {
+                //  $('.calendari__setmana:last-child').children('.calendari__dia:last-child').append(tasquesDiaPlantilla({tasques : data}));
+                // })
+            }
+        }
     })();
 
     // Llibreta tatxar
@@ -50,6 +88,13 @@ $(document).ready(function() {
 
 });
 
-const setmanaPlantilla = ({}) => `
-<div class="calendari__setmana row ms-0 me-0"></div>
+const setmanaPlantilla = () => `
+<div class="calendari__setmana row ms-0 me-0">
+</div>
+`;
+
+const diaPlantilla = ({numDia}) => `
+    <div class="col calendari__dia">
+        <span>${numDia}</span>
+    </div>
 `;

4649e1e7302ecf0c747d0c8a0934fd5fae83be98
Xope <errchosep25@gmail.com>
2023-02-20 09:15:17 +0100

    Fets controladors page i centraleta

---
+++ src/Controller/CentraletaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CentraletaController extends AbstractController
+{
+    #[Route('/centraleta', name: 'centraleta')]
+    public function index(): Response
+    {
+        return $this->render('centraleta/index.html.twig', [
+            'controller_name' => 'CentraletaController',
+        ]);
+    }
+}
